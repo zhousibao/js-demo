c3ed6b6b45d164ef7f02b749412e1a43442892b6
zhousibao <pan.zhou@zhangmen.com>
2020-08-18 10:37:33 +0800

    防抖

--- src/节流防抖.js
+++ src/节流防抖.js
@@ -35,3 +35,30 @@ const fn1 = throttle1(() => console.log('fn1 函数执行了'), 1000)
 const fn2 = throttle2(() => console.log('fn2 函数执行了'), 1000)
 setInterval(fn1, 10)
 setInterval(fn2, 10)
+
+
+/**
+ * @description 防抖函数
+ * @param {function} fn 需要处理的函数
+ * @param {number} delay 防抖时间
+ * @param {boolean} immediate 是否立即执行一次
+ */
+const debounce = (fn, delay = 200, immediate = false) => {
+  let timeOut = null
+  return (...rest) => {
+    // 存在定时器，则直接清空
+    if (timeOut) clearTimeout(timeOut)
+
+    // 是否立即执行一次
+    if (immediate && !timeOut) {
+      fn.apply(this, rest)
+    }
+
+    timeOut = setTimeout(() => {
+      fn.apply(this, rest)
+    }, delay)
+  }
+}
+
+var input = document.getElementById('input')
+input.addEventListener('input', debounce(() => console.log('fn2 函数执行了'), 1000))
