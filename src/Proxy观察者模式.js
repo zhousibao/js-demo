5953a14038d9ca16d93a22a4631bdf6ddf82aed5
zhousibao <pan.zhou@zhangmen.com>
2020-08-18 16:52:29 +0800

    观察者模式

---
+++ src/Proxy观察者模式.js
@@ -0,0 +1,48 @@
+/**
+ * @观察者模式 new Proxy(target, handler)
+ * @target 表示要拦截的目标对象
+ * @handler 也是一个对象，用来定制拦截行为。
+ *
+ */
+
+
+// 存储action
+const actionList = new Set()
+// 添加方法
+const addAction = fn => actionList.add(fn)
+// 拦截行为对象
+const handle = {
+  get: (target, key, receiver) => {
+    // target目标对象 receiver是Proxy实例
+    const val = Reflect.get(target, key, receiver)
+    actionList.forEach(action => action(target)) // 调用观察者自定义的方法
+    return val
+  },
+  set: (target, key, val, receiver) => {
+    const oldVal = Reflect.get(target, key, receiver)
+
+    if (oldVal !== val) {
+      Reflect.set(target, key, val, receiver)
+      actionList.forEach(action => action(target))
+    }
+  },
+}
+// 观察者函数
+const Observer = obj => new Proxy(obj, handle)
+
+
+// 例子
+const obj = {
+  name: '张三',
+  age: 20,
+}
+function print (target) {
+  console.log(`print: ${target.name}, ${target.age}`)
+}
+function write (target) {
+  console.log(`write: ${target.name}, ${target.age}`)
+}
+const person = Observer(obj)
+addAction(print)
+addAction(write)
+person.name = '李四'
