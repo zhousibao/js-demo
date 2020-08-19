a61c6f2908ec65b0e2e2e763812df7ba67cc3491
zhousibao <pan.zhou@zhangmen.com>
2020-08-19 10:30:29 +0800

    斐波那契数列

---
+++ src/斐波那契数列.js
@@ -0,0 +1,16 @@
+/**
+ * @斐波那契数列 前两个数之和等于第三个数
+ */
+
+function * fibonacci () {
+  let [prev, curr] = [0, 1]
+  for (;;) {
+    yield curr; // 注意yield后面加分号
+    [prev, curr] = [curr, prev + curr] // 利用数组结构赋值
+  }
+}
+
+for (const n of fibonacci()) {
+  if (n > 100) break
+  console.log(n)
+}
