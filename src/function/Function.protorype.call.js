ea0dda91502d035f2f8268b41a08914dd36eca2e
zhousibao <pan.zhou@zhangmen.com>
2020-08-29 17:11:48 +0800

    call

---
+++ src/function/Function.protorype.call.js
@@ -0,0 +1,34 @@
+/**
+ * Function.prototype.call(thisArg, arg1, arg2, ...)
+ * 方法使用一个指定的 thisArg 值和单独给出的一个或多个参数来调用一个函数
+ *
+ * @实现对象的继承
+ * @改变函数的上下文环境
+ * @立即执行
+ */
+
+// eslint-disable-next-line
+Function.prototype._call = function (thisArg) {
+  const context = thisArg || window
+  context.fn = this // this表示当前函数
+  const args = [...arguments].slice(1) // 得到其他参数
+  const result = context.fn(...args)
+  delete context.fn
+  return result
+}
+
+var name = 'pan'
+console.log(name) // 'pan
+
+const obj = {
+  name: 'zhou',
+}
+function hello () {
+  return this.name
+}
+
+hello.call() // 'pan'
+hello._call() // 'pan'
+
+hello.call(obj) // 'zhou'
+hello._call(obj) // 'zhou'
