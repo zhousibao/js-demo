910ecd213b5066c87c0340e9dad34cebd8c6c1a9
zhoupan <zhoupan@theduapp.com>
2020-11-18 11:28:39 +0800

    缓存代理

--- src/designPatterns/代理模式.js
+++ src/designPatterns/代理模式.js
@@ -10,6 +10,7 @@
 
 /**
  * @保护代理
+ * 限制访问主体的行为
  */
 // 主体，发送消息
 function sendMsg (msg) {
@@ -32,7 +33,8 @@ proxySendMsg() // deny
 
 
 /**
- * 虚拟代理
+ * @虚拟代理
+ * 控制对主体的访问时，加入了一些额外的操作
  */
 // 函数防抖，频繁操作中不处理，直到操作完成之后（再过 delay 的时间）才一次性处理
 const debounce = (fn, delay = 200, immediate = false) => {
@@ -63,3 +65,33 @@ var proxyScrollHandle = (function () {
 })()
 window.onscroll = proxyScrollHandle
 
+
+/**
+ * @缓存代理
+ * 可以为一些开销大的运算结果提供暂时的缓存，提升效率
+ */
+// 主体
+function add () {
+  console.log('执行了add')
+  const arg = Array.prototype.slice.call(arguments)
+  return arg.reduce((a, b) => a + b)
+}
+// 代理
+var proxyAdd = (function () {
+  const caches = new Map() // 闭包实现缓存数据
+
+  return function () {
+    console.log(caches)
+    const arg = Array.prototype.slice.call(arguments).join(',')
+
+    if (caches.has(arg)) {
+      return caches.get(arg)
+    }
+
+    const result = add.apply(this, arguments)
+    caches.set(arg, result)
+    return result
+  }
+})()
+proxyAdd(10, 20, 30, 40) // 执行add函数
+proxyAdd(10, 20, 30, 40) // 直接读取缓存
