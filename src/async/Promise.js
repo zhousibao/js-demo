ca81bc63505f87e5ddf7844da1bb7a35213a180c
zhoupan <zhoupan@theduapp.com>
2020-11-27 11:23:18 +0800

    promise实例方法

--- src/async/Promise.js
+++ src/async/Promise.js
@@ -1,6 +1,13 @@
 /**
  * @手写Promise
  * 参考 https://juejin.cn/post/6899273470623318023
+ *
+ * @特点
+ * 1、有三个状态：Pending进行中、Fulfilled已成功、Rejected已失败。外界无法改变状态，一旦改变无法再改变。
+ * 2、构造函数接收 executor 函数作为参数
+ * 3、构造函数中有 resolve 和 reject 内置方法，并作为参数传递给 executor 函数。
+ * 4、实例方法then: 微任务队列、支持链式调用
+ * 5、实例方法catch: 就是then(null, onRejected)的别名
  */
 
 // 用Symbol定义三种状态，防止外界改变状态。
@@ -38,42 +45,172 @@ class _Promise {
         this.onRejected.forEach(fn => fn())
       }
     }
-    executor(resolve, reject)
+
+    // try ... catch 语句来捕获错误
+    try {
+      executor(resolve, reject)
+    } catch (error) {
+      reject(error)
+    }
   }
 
   // then实例方法
   then (onFulfilled, onRejected) {
-    if (this.status === Fulfilled) {
-      if (onFulfilled && typeof onFulfilled === 'function') {
-        // 使用 setTimeout 模拟异步，所以原生的是微任务，这里是宏任务
-        setTimeout(() => { onFulfilled(this.value) }, 0)
+    // 实现then透传
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err }
+
+
+    // 实现链式调用的原理，返回一个新的 Promise 对象
+    const promise = new _Promise((resolve, reject) => {
+      if (this.status === Fulfilled) {
+        if (onFulfilled && typeof onFulfilled === 'function') {
+          // 使用 setTimeout 模拟异步，所以原生的是微任务，这里是宏任务
+          setTimeout(() => {
+            try {
+              const x = onFulfilled(this.value)
+              handleValue(promise, x, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          }, 0)
+        }
       }
-    }
-    if (this.status === Rejected) {
-      if (onRejected && typeof onRejected === 'function') {
-        setTimeout(() => { onRejected(this.reason) }, 0)
+      if (this.status === Rejected) {
+        if (onRejected && typeof onRejected === 'function') {
+          setTimeout(() => {
+            try {
+              const x = onRejected(this.reason)
+              handleValue(promise, x, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          }, 0)
+        }
       }
-    }
-    if (this.status === Pending) {
-      if (onFulfilled && typeof onFulfilled === 'function') {
-        this.onFulfilled.push(() => {
-          setTimeout(() => { onFulfilled(this.value) }, 0)
-        })
+
+      // promise内部为异步时
+      if (this.status === Pending) {
+        if (onFulfilled && typeof onFulfilled === 'function') {
+          this.onFulfilled.push(() => {
+            setTimeout(() => {
+              try {
+                const x = onFulfilled(this.value)
+                handleValue(promise, x, resolve, reject)
+              } catch (error) {
+                reject(error)
+              }
+            }, 0)
+          })
+        }
+        if (onRejected && typeof onRejected === 'function') {
+          this.onRejected.push(() => {
+            setTimeout(() => {
+              try {
+                const x = onRejected(this.reason)
+                handleValue(promise, x, resolve, reject)
+              } catch (error) {
+                reject(error)
+              }
+            }, 0)
+          })
+        }
       }
-      if (onRejected && typeof onRejected === 'function') {
-        this.onRejected.push(() => {
-          setTimeout(() => { onRejected(this.reason) }, 0)
-        })
-      }
-    }
+    })
+
+    return promise
+  }
+
+  // catch实例方法
+  catch (onRejected) {
+    this.then(null, onRejected)
   }
 }
 
-const test = new _Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('"执行成功"')
-  }, 1000)
+
+const handleValue = (promise, x, resolve, reject) => {
+  // 循环引用，自己等待自己完成，会出错，用reject传递出错误原因
+  if (promise === x) {
+    return reject(new TypeError('检测到Promise的链式循环引用'))
+  }
+  // 确保只传递出去一次值
+  let once = false
+  if ((x !== null && typeof x === 'object') || typeof x === 'function') {
+    // try ... catch 语句来捕获错误
+    try {
+    // 防止重复去读取x.then
+      const then = x.then
+      // 判断x是不是Promise
+      if (typeof then === 'function') {
+      // 调用then实例方法处理Promise执行结果
+        then.call(x, y => {
+          if (once) return
+          once = true
+          // 防止Promise中Promise执行成功后又传递一个Promise过来，
+          // 要做递归解析。
+          handleValue(promise, y, resolve, reject)
+        }, r => {
+          if (once) return
+          once = true
+          reject(r)
+        })
+      } else {
+      // 如果x是个普通对象，直接调用resolve(x)
+        resolve(x)
+      }
+    } catch (err) {
+      if (once) return
+      once = true
+      reject(err)
+    }
+  } else {
+    // 如果x是个原始值，直接调用resolve(x)
+    resolve(x)
+  }
+}
+
+
+// 测试同步操作
+const test1 = new _Promise((resolve, reject) => {
+  resolve('测试同步操作')
 })
-test.then(res => {
+test1.then(res => {
+  console.log(res)
+})
+
+// 测试异步操作
+const test2 = new _Promise((resolve, reject) => {
+  setTimeout(() => { resolve('测试异步操作') }, 1000)
+})
+test2.then(res => {
+  console.log(res)
+})
+
+// 测试then链式操作
+const test3 = new _Promise((resolve, reject) => {
+  setTimeout(() => { resolve('测试then链式操作') }, 2000)
+})
+test3.then(res => {
+  console.log(res)
+  return 'thenable'
+}).then(res => {
+  console.log(res)
+})
+
+
+// 测试then透传
+const test4 = new _Promise((resolve, reject) => {
+  setTimeout(() => { resolve('测试then透传') }, 3000)
+})
+test4.then().then(res => {
+  console.log(res)
+})
+
+
+// 测试catch
+const test5 = new _Promise((resolve, reject) => {
+  setTimeout(() => { reject('测试catch') }, 4000)
+})
+test5.then().catch(res => {
   console.log(res)
 })
